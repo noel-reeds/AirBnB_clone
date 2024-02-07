09689f7a6af941f024635ca8301da5aea73cd870
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-02-07 13:06:34 +0000

    console, entry point of the command interpreter

---
+++ console.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+    intro = "Hello, type 'help' and start exploring.."
+    prompt = "(hbnb)"
+
+    def do_quit(self, *args):
+        """Exits the console."""
+        print("Thank you and goodbye!")
+        return True
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
