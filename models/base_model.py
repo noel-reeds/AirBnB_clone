a218bef8a7d1c1092f4317cf023f781d85c5082c
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-02-17 11:53:07 +0000

    converted to string object in ISO format

--- models/base_model.py
+++ models/base_model.py
@@ -8,13 +8,13 @@ class BaseModel():
     """Defines all common attr./methods for other classes"""
     def __init__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
-        self.created_at = datetime.now().isoformat
-        self.updated_at = datetime.now().isoformat
+        self.created_at = datetime.now().isoformat()
+        self.updated_at = datetime.now().isoformat()
 
     def save(self):
         """ updates the public instance attribute updated_at
         with the current datetime"""
-        self.updated_at = datetime.now().isoformat
+        self.updated_at = datetime.now().isoformat()
 
     def to_dict(self):
         """returns a dictionary containing all keys/values
