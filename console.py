56ce32849bc21bb879b80a030a242142c1785974
Noel Reeds <141303207+voidptr0xff@users.noreply.github.com>
2024-02-18 06:45:25 +0000

    added more methods to the console

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import cmd
 from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+import sys
 
 
 class HBNBCommand(cmd.Cmd):
@@ -19,8 +21,53 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, *args):
         """Creates an instance of BaseModel"""
+        if args == 1:
+            print("** class name missing **")
+        elif sys.argv[1] is not BaseModel:
+            print("** class doesn't exist **")
         obj = BaseModel()
+        FileStorage.save(obj)
+        print(obj.id)
 
+    def do_show(self, *args):
+        """Prints a str rep of an instance based \
+on class name and id."""
+        if args == 1:
+            print("** class name missing **")
+        elif sys.argv[1] is not BaseModel:
+            print("** class doesn't exist **")
+        elif args == 2:
+            print("** instance id missing **")
+        elif sys.argv[2] not in object_dict.keys():
+            print("** no instance found **")
+        return object_dict[sys.argv[2]]
 
+    def do_destroy(self, args):
+        """Deletes an instance based on the class name and id"""
+        if args == 1:
+            print("** class name missing")
+        elif sys.argv[1] is not BaseModel:
+            print("** class doesn't exist **")
+        if args == 2:
+            print("** instance id missing **")
+        elif sys.argv[2] not in object_dict.keys():
+            print("** no instance found **")
+        del object_dict[sys.argv[2]]
+        BaseModel.save(self)
+
+    def do_all(self, *args):
+        """Prints str representation of all \
+instances based or not on the class name"""
+        if args == 2 and sys.argv[1] is not BaseModel:
+            print("** class doesn't exist **")
+        elif args == 1 and sys.argv[0] == "all":
+            print("** class doesn't exist **")
+        else:
+            final_dict = BaseModel.to_dict(self)
+            return final_dict
+
+    def do_update(self, *args):
+        """Updates an instance based on the class name and id"""
+        pass
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
