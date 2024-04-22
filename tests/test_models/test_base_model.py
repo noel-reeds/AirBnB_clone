a628e350f8b50e3293862144e923b1c848fd7a31
altoid <noelreeds01@gmail.com>
2024-04-22 14:32:16 +0000

    Tests for save instance method

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,11 +2,7 @@
 """Tests 'Base Model' module"""
 import unittest
 from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.state import State
-from models.review import Review
+from datetime import datetime
 from models.user import User
 
 
@@ -15,18 +11,8 @@ class TestBaseModel(unittest.TestCase):
     def test_isinstance(self):
         """Tests for isinstance"""
         base = BaseModel()
-        amenity = Amenity()
-        city = City()
-        place = Place()
-        state = State()
-        review = Review()
         user = User()
         self.assertIsInstance(base, BaseModel)
-        self.assertIsInstance(amenity, Amenity)
-        self.assertIsInstance(city, City)
-        self.assertIsInstance(place, Place)
-        self.assertIsInstance(state, State)
-        self.assertIsInstance(review, Review)
         self.assertIsInstance(user, User)
 
     def test_attrs(self):
@@ -36,3 +22,8 @@ class TestBaseModel(unittest.TestCase):
         self.assertIn('id', base.__dict__.keys())
         self.assertIn('created_at', base.__dict__.keys())
         self.assertIn('updated_at', base.__dict__.keys())
+
+    def test_methods(self):
+        base = BaseModel()
+        res = base.save()
+        self.assertIsInstance(base.updated_at, datetime)
