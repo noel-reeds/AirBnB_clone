69ed1743930f021d9d7025b433d868b8730aa7b5
altoid <noelreeds01@gmail.com>
2024-04-22 12:01:18 +0000

    updates attrs to datetime

--- models/base_model.py
+++ models/base_model.py
@@ -11,11 +11,9 @@ class BaseModel:
             if "__class__" in kwargs.keys():
                 del kwargs["__class__"]
             if "created_at" in kwargs.keys():
-                kwargs["created_at"] = datetime.\
-                    strptime(kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                kwargs["created_at"] = datetime.fromisoformat(kwargs["created_at"])
             if "updated_at" in kwargs.keys():
-                kwargs["updated_at"] = datetime.\
-                    strptime(kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
+                kwargs["updated_at"] = datetime.fromisoformat(kwargs["updated_at"])
             for key, value in kwargs.items():
                 setattr(self, key, value)
         else:
@@ -38,8 +36,8 @@ class BaseModel:
     def to_dict(self):
         """returns a dictionary containing all keys/values
         of __dict__ of the instance"""
-        self.created_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.updated_at = datetime.now().strftime("%Y-%m-%dT%H:%M:%S.%f")
-        object_dict = self.__dict__.copy()
-        object_dict['__class__'] = self.__class__.__name__
-        return object_dict
+        new_dict = dict(self.__dict__)
+        new_dict['created_at'] = new_dict['created_at'].isoformat()
+        new_dict['updated_at'] = new_dict['updated_at'].isoformat()
+        new_dict['__class__'] = self.__class__.__name__
+        return new_dict
