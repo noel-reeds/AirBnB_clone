e0c860761d908090d67eef07d2a3f110ec6023cd
altoid <noelreeds01@gmail.com>
2024-04-22 13:45:56 +0000

    Tests for isinstance

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,11 +2,29 @@
 """Tests 'Base Model' module"""
 import unittest
 from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.state import State
+from models.review import Review
+from models.user import User
 
 
 class TestBaseModel(unittest.TestCase):
     "Tests case for the base model module"""
-    def test_attrs(self):
-        """Tests existence of instance attrs"""
-        obj = BaseModel()
-        self.assertIsInstance(obj, BaseModel)
+    def test_isinstance(self):
+        """Tests for isinstance"""
+        base = BaseModel()
+        amenity = Amenity()
+        city = City()
+        place = Place()
+        state = State()
+        review = Review()
+        user = User()
+        self.assertIsInstance(base, BaseModel)
+        self.assertIsInstance(amenity, Amenity)
+        self.assertIsInstance(city, City)
+        self.assertIsInstance(place, Place)
+        self.assertIsInstance(state, State)
+        self.assertIsInstance(review, Review)
+        self.assertIsInstance(user, User)
