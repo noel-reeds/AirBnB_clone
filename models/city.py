37d17db8784ccb5cc22e3b0acbec62721722dd71
altoid <noelreeds01@gmail.com>
2024-10-31 14:23:06 +0000

    added name attr

--- models/city.py
+++ models/city.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     state_id = ""
+    name = ""
 
     def __init__(self, *args, **kwargs):
         """instantiates a city model"""
