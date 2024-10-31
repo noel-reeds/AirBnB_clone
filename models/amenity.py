829c405340d7a89b03a232becc7b0229cb396bd8
altoid <noelreeds01@gmail.com>
2024-10-31 10:35:58 +0000

    initializes amenity cls

--- models/amenity.py
+++ models/amenity.py
@@ -5,3 +5,7 @@ from models.base_model import BaseModel
 
 class Amenity(BaseModel):
     name = ""
+
+    def __init__(self, *args, **kwargs):
+        """instantiates an amenity model"""
+        super().__init__(*args, **kwargs)
